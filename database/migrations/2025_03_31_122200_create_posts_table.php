4694884af19bd3c0b4359e4134dd2ce227247e95
Isomiddin <isomiddinmamadaminov133@gmail.com>
2025-04-02 09:04:56 +0500

    migrations are written here

--- database/migrations/2025_03_31_122200_create_posts_table.php
+++ database/migrations/2025_03_31_122200_create_posts_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('body');
             $table->string('image')->nullable()->change();
             $table->timestamps();
-            
+        
         });
     }
 
